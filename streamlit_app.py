50cede459936c8010d986bd7b0910a88e9f73ecf
Kenny Jade <ntdkhoa@hotmail.com>
2024-11-04 10:42:19 +0700

    20241104_230000

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,76 @@
 import streamlit as st
+import datetime as dt
 import pandas as pd
-import math
-from pathlib import Path
+import pandas_ta as ta
+import numpy as np
+import time
+import yaml
+import altair as alt
+import requests
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+# Load YAML configuration
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+today = dt.datetime.now()
+starttime = round(time.mktime((today.year, today.month, today.day, 9, 0, 0, 0, 0, 0)))
+endtime = round(time.time()) + 60
+buy_period = 19
+sell_period = 4
+b_shield = 0.00
+s_shield = 0.00
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+def get_intraday_data(stock_name, interval):
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+    header = config['headers']['vietstock']['header_0']
+    url = config['urls']['vietstock']['history'] + stock_name + '&resolution=' + interval + '&from=' + str(starttime) + '&to=' + str(endtime)        
+    response = requests.get(url, headers = header)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    if response.status_code == 200:
+        data = response.json()
+        df = pd.DataFrame(data)
+        df['time'] = df['t'].apply(lambda x: dt.datetime.fromtimestamp(x).strftime('%H:%M'))
+        df['average'] = df[['h', 'l', 'o', 'c']].mean(axis=1)
+        df['HullMA_Short'] = round(ta.hma(df['average'], length=buy_period), 1)
+        df['HullMA_Long'] = round(ta.hma(df['average'], length=sell_period), 1)
+        df['average'] = round(df['average'], 1)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+        conditions = [
+            ((df['average'] - b_shield) > df['HullMA_Short']) & ((df['average'] - b_shield) > df['HullMA_Long']),  # Buy
+            ((df['average'] + s_shield) < df['HullMA_Long'])  # Sell
+        ]
+        choices = [1, -1] # Define choices corresponding to the conditions: 1 for buy, -1 for sell
+        df['Signal'] = np.select(conditions, choices, default=0)
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+        return df
+    else:
+        print("error request")
+    return df
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+# Set page configuration
+st.set_page_config(page_title="Intraday Stock Price Tracker", layout="wide")
 
-    return gdp_df
+# Function to fetch data for a specific stock symbol
+#@st.cache_data(ttl=60)  # Cache the data for 60 seconds
 
-gdp_df = get_gdp_data()
+# Function to render chart
+#def render_chart(data):
+#    st.line_chart(data[['t', 'c']].set_index('t'))
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+# JavaScript code to auto-refresh the specific component every 60 seconds
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
+# Display stock data
+st.title("Intraday Stock Price Tracker for AAPL")
+st.write("Auto-refreshing every 60 seconds")
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
+# Placeholder for the chart
+#chart_placeholder = st.empty()
 
-# Add some spacing
-''
-''
+# Load data and render the chart initially
+data = get_intraday_data('VN30F1M', '1')
+st.write(data)
+#with chart_placeholder:
+#    render_chart(data)
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+# Inject the JavaScript for auto-refresh
+#st.components.v1.html(auto_refresh_code)
